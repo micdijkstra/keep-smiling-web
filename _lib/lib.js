090b15be9816d2533dd6ec9021cc351814c358df
jheftmann <jheftmann@mac.com>
2014-10-04 16:56:27 -0400

    recompile lib js

--- _lib/lib.js
+++ _lib/lib.js
@@ -5,14 +5,3 @@
 // @codekit-prepend "js/crossbrowser-fixes.js"
 // @codekit-prepend "js/html5-placeholder-shim.js"
 // @codekit-prepend "js/less.js"
-
-/* plugins */
-// @codekit-prepend "js/waypoints.min.js"
-// @codekit-prepend "js/fitvids.js"
-
-/* v0 */
-// @codekit-prepend "js/v0-scroll-top.js"
-// @codekit-prepend "js/v0-window-height.js"
-// @codekit-prepend "js/v0-window-orientation.js"
-// @codekit-prepend "js/v0-window-size.js"
-// @codekit-prepend "js/v0-load-page.js"
