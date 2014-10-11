9c5a29e473b379ac5f502c56337e37e960fe3eb3
Michael Dijkstra <micdijkstra@gmail.com>
2014-10-12 07:53:37 +1100

    Tweaks
    + Play/Pause video as you scroll
    + Swap back colors (Closes #5)
    + Swap nav colors and border
    + Adjust sidebar spacing on smaller screens
    + Adjust Tap Tap Tap line height on smaller screens
    + Remove line breaks (Closes #4)
    + Fix nav when you scroll back up

--- scripts/site.js
+++ scripts/site.js
@@ -1,70 +1,113 @@
 $(document).ready(function() {
 
-	// center content within sections
+  // center content within sections
   $(window).resize(function() {
 
-	 if ($(window).width() >= 480) {
-	    $('.section').each(function(){
-	      var parent = $(this);
-	      var child = $(this).find('.content-middle');
-	      var topMargin = ((parent.height() - child.height()) / 2);
+  if ($(window).width() >= 480) {
+      $('.section').each(function(){
+        var parent = $(this);
+        var child = $(this).find('.content-middle');
+        var topMargin = ((parent.height() - child.height()) / 2);
 
-	      child.css('margin-top', topMargin);
+        child.css('margin-top', topMargin);
 
-	      console.log(parent);
-	      console.log(child);
+        console.log(parent);
+        console.log(child);
 
-	    });
-   } else {
+      });
+  } else {
 
-   }
+  }
 
   });
 
 
 
-	// scrolling nav
-	$('.nav').onePageNav({
-	    currentClass: 'current',
-	    changeHash: false,
-	    scrollSpeed: 750,
-	    scrollThreshold: 0.5,
-	    filter: '',
-	    easing: 'swing',
-	    begin: function() {
-	        //I get fired when the animation is starting
-	    },
-	    end: function() {
-	        //I get fired when the animation is ending
-	    },
-	    scrollChange: function($currentListItem) {
-	        //I get fired when you enter a section and I pass the list item of the section
-	    }
-	});
+  // scrolling nav
+  $('.nav').onePageNav({
+      currentClass: 'current',
+      changeHash: false,
+      scrollSpeed: 750,
+      scrollThreshold: 0.5,
+      filter: '',
+      easing: 'swing',
+      begin: function() {
+          //I get fired when the animation is starting
+      },
+      end: function() {
+          //I get fired when the animation is ending
+      },
+      scrollChange: function($currentListItem) {
+          //I get fired when you enter a section and I pass the list item of the section
+      }
+  });
 
 
-	// scroll to next section
-	$('.next-section').click(function() {
-		$('html, body').animate({
-		scrollTop: $("#section-2").offset().top
-	}, 500);
-	});
+  // scroll to next section
+  $('.next-section').click(function() {
+    $('html, body').animate({
+    scrollTop: $("#section-2").offset().top
+  }, 500);
+  });
 
 
-	// adjust some things based on the current section
-	// fix nav between sections 2 and 6
-	$('#section-2').waypoint(function(direction) {
-	  $('.nav').addClass('fixed');
-	});
+  // adjust some things based on the current section
+  // fix nav between sections 2 and 6
+  $('#section-2').waypoint(function(direction) {
+    if (direction === 'down') {
+      $('.nav').addClass('fixed');
+    }
+  }).waypoint(function(direction) {
+    if (direction === 'up') {
+      $('.nav').removeClass('fixed');
+    }
+  });
 
-	// swap colors over dark sections
-	$('.dark').waypoint(function(direction) {
-	  $('.swap').addClass('text-yellow');
-	}, { offset: '50%' });
+  // swap colors over dark sections
+  $('.dark').waypoint(function(direction) {
+    if (direction === 'down') {
+      $('.swap').addClass('text-yellow');
+    }
+  }, { offset: '50%' }).waypoint(function(direction) {
+    if (direction === 'up') {
+      $('.swap').addClass('text-yellow');
+    }
+  }, { offset: '-50%' });
+
+  $('.light').waypoint(function(direction) {
+    if (direction === 'down') {
+      $('.swap').removeClass('text-yellow');
+    }
+  }, { offset: '50%' }).waypoint(function(direction) {
+    if (direction === 'up') {
+      $('.swap').removeClass('text-yellow');
+    }
+  }, { offset: '-50%' });
+
+  // play video when it enters viewport
+  $('#section-3').waypoint(function(direction) {
+    if (direction === 'down') {
+      $('#video').get(0).play();
+    }
+  }, { offset: '50%' }).waypoint(function(direction) {
+    if (direction === 'up') {
+      $('#video').get(0).play();
+    }
+  }, { offset: '-50%' });
+
+  // pause video when it leaves viewport
+  $('#section-4').waypoint(function(direction) {
+    if (direction === 'down') {
+      $('#video').get(0).pause();
+    }
+  }, { offset: '25%' })
+
+  $('#section-3').waypoint(function(direction) {
+    if (direction === 'up') {
+      $('#video').get(0).pause();
+    }
+  }, { offset: '75%' });
 
 
-	// play video when it enters viewport
-
-
-	$(window).trigger('resize');
+  $(window).trigger('resize');
 });
